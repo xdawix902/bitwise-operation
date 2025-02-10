031cadc1a1264e3d3e875c7333e3f759df60210e
xdawix902 <shigole@wp.pl>
2025-02-10 19:09:32 +0100

    2021 zadanie z bitwise operation

---
+++ Zadania_z_dysku/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Zadania_z_dysku;
+
+class Program
+{
+    static long Bitewise2021(long n){
+        long maska = n & 0b111100000000;
+        maska = maska >> 7;
+        if(maska == 0b1111){
+            maska = 0;
+        }
+        else{
+            maska = maska + 1;
+        }
+        maska = maska << 7;
+        return n | maska;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
