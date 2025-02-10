46b4cd0c5ad19dc267dc3d5e681d00ac53c6336b
xdawix902 <shigole@wp.pl>
2025-02-10 21:07:55 +0100

    zadania z bitowych z bazy

--- Zadania_z_dysku/Program.cs
+++ Zadania_z_dysku/Program.cs
@@ -2,6 +2,40 @@
 
 class Program
 {
+    static byte S3(int rejestr, byte wzor){
+        byte counter = 0;
+        while(rejestr > 0){
+            int maska = 0b11;
+            maska = maska & rejestr;
+            if(maska == wzor) counter ++;
+            rejestr = rejestr >> 1;
+        }
+        return counter;
+    }
+    static long Bitwise2018_2(long n){
+        long maska = 0b1110000000;
+        maska = maska & n;
+        maska = maska >> 6;
+        if(maska == 0){
+            maska = 0b111;
+        }else{
+            maska = maska - 1;
+        }
+        maska = maska << 6;
+        return maska;
+    }
+    static long Bitwise2018_1(long n){
+        long maska = 0b11110000000;
+        maska = maska & n;
+        maska = maska >> 6;
+        if(maska == 0b1111){
+            maska = 0;
+        }else{
+            maska += 1;
+        }
+        maska = maska << 6;
+        return n | maska;
+    }
     static long Bitewise2021(long n){
         long maska = n & 0b111100000000;
         maska = maska >> 7;
