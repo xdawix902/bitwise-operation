7962d86b73edc76315e8f2c0d37c002fcf46038d
xdawix902 <shigole@wp.pl>
2025-02-10 21:50:32 +0100

    zadania z deepseeka

--- Zadania_z_dysku/Program.cs
+++ Zadania_z_dysku/Program.cs
@@ -2,6 +2,51 @@
 
 class Program
 {
+    // uint = 16 bitów
+    // int = 32 bity
+    // ulong = 32 bity
+    // long = 64 bity
+
+
+    //Zadanie: Zadanie: Liczenie izolowanych bitów
+    static int CountIsolatedBits(int liczba){
+        int counter = 0;
+        while(liczba > 0){
+            int maska = liczba & 0b111;
+            if(maska == 0b010) counter ++;
+            liczba = liczba >> 1;
+        }
+        return counter;
+    }
+
+
+    //Zadanie: Zamiana bitów w parach
+    static int SwapBitsInPairs(int liczba){
+        int odp = 0;
+        int i = 0;
+        while(liczba > 0){
+            int maska = 0b11;
+            int maska_liczby = liczba & 0b11;
+            maska = (maska ^ maska_liczby);
+            odp = odp | (maska << 2*i);
+            liczba = liczba >> 2;
+            i++;
+        }
+        return odp;
+    }
+
+    //Zadanie: Liczenie odwróconych par bitów
+    static int CounterInvertedPairs(int liczba){
+        int counter = 0;
+        long wzor1 = 0b10;
+        long wzor2 = 0b01;
+        while(liczba > 0){
+            long maska = liczba & 0b11;
+            if(maska == wzor1 || maska == wzor2) counter ++;
+            liczba = liczba >> 1;
+        }
+        return counter;
+    }
     static byte S3(int rejestr, byte wzor){
         byte counter = 0;
         while(rejestr > 0){
